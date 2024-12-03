ff1489b7a015999b7bb529dba739f0f609edbf00
Jonny Jackson <jrsjackson26@gmail.com>
2024-12-03 14:22:58 -0700

    blue pixel in top left on s key, behind red background

--- assembly_file.asm
+++ assembly_file.asm
@@ -1,35 +1,49 @@
 .data
+# Screen info
 screenStart: 16384    # 0x4000
 screenEnd: 24576      # 0x6000
 colorRed: 63488 
+colorGreen: 2016
+colorBlue: 31
+
+# Keyboard info
+keyboardMem: 24576
+upArrow: 38
+downArrow: 40
+wKey: 87
+sKey: 83
+spaceKey: 32
 
 .text
 j main
 
-
 main:
+    lw R1, screenStart    # Screen start address
+    lw R2, screenEnd      # Screen end address
+    lw R3, colorRed       # Initial color (red)
 
-lw R1, screenStart
-lw R2, screenEnd
-lw R3, colorRed #R3 holds color
-
-drawLoop:
-    beq R1, R2, drawEnd
-
+makeScreenRed:
     # Draw pixel to current address
     sw R3, 0(R1)
-
-    # Add one to the address and color and jump to top
-    addi R1, R1, 1
-
-    # Update the screen every pixel we draw
+    addi R1, R1, 1        # Move to next address
     display
+    
+    # Check if we've reached the end of the screen
+    beq R1, R2, drawEnd
 
-    j drawLoop
-
-
+    j makeScreenRed
 
 drawEnd:
-    # Reset the address pointer to the start of the screen
+    lw R4, keyboardMem    # Load keyboard memory value
+    lw R4, 0(R4)
+    lw R5, sKey           # Load "S" key value
+    bne R4, R5, drawEnd               # Restart the main loop
+    
+
+
+setBlue:
     lw R1, screenStart
-    j drawLoop
+    lw R3, colorBlue      # Change current color to blue
+    sw R3, 0(R1)
+    display
+     
--- main.py
+++ main.py
@@ -216,21 +216,21 @@ def main():
 
     # Step 3: Build a label table and strip out the labels from the code
     label_table, lines = create_label_table(lines)
-    print(label_table)
-    print(lines)
+    #print(label_table)
+    #print(lines)
    
     # Step 4: Encode the program into a list of binary strings
     encoded_program = encode_program(lines, label_table, data_table)
-    #print_lines(encoded_program)
+    print_lines(encoded_program)
 
     # Step 5: Convert the strings to hexadecimal and write them to a file
     hex_program = post_process(encoded_program)
-    with open("output.hex", "w") as outfile:
+    with open("rom.hex", "w") as outfile:
         outfile.write("v3.0 hex words addressed\n00: ")
         outfile.writelines(hex_program)
 
     # Step 6: Convert the data list to hexadecimal and write it to a file
-    with open("data.hex", "w") as outfile:
+    with open("ram.hex", "w") as outfile:
         outfile.write("v3.0 hex words addressed\n00: ")
         outfile.writelines([f"{d:04x} " for d in data_list])
 
